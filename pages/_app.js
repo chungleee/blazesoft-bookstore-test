dbbc10f7cd75e367f7a82f837c2aa31821918dc0
chungleee <leon.karho.chung@gmail.com>
2022-06-25 16:04:40 -0400

    provide redux store and import global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { wrapper } from "../redux/store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+import '../css/globals.css';
 
 const App = ({ Component, props }) => {
-	return <Component {...props} />;
+	return (
+		<Provider store={store}>
+			<Component {...props} />;
+		</Provider>
+	);
 };
 
-export default wrapper.withRedux(App);
+export default App;
