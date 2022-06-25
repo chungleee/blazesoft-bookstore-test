21e268cdcd6dcb4f0517297c4201afacdc423840
chungleee <leon.karho.chung@gmail.com>
2022-06-25 16:05:54 -0400

    redux store with books reducer

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createWrapper } from "next-redux-wrapper";
-import booksReducer from "./features/books/booksSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './features/books/booksSlice';
 
-const makeStore = () => {
-	return configureStore({
-		reducer: {
-			books: booksReducer,
-		},
-	});
-};
-
-export const wrapper = createWrapper(makeStore);
+export const store = configureStore({
+	reducer: {
+		books: booksReducer,
+	},
+});
