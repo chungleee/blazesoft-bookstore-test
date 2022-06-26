ae13a333455e16af1c1bdd4489e5c31c57da79e2
chungleee <leon.karho.chung@gmail.com>
2022-06-26 15:41:04 -0400

    popup toggle state and toggle handler function

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import { useDispatch, useSelector, useStore } from 'react-redux';
-import BookCard from '../components/BookCard';
-import { addBooks } from '../redux/features/books/booksSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import BookCard from "../components/BookCard";
+import Button from "../components/Button";
+import Popup from "../components/Popup";
+import { addBooks } from "../redux/features/books/booksSlice";
 
-const HomePage = (props) => {
+const HomePage = () => {
+	const [popupToggle, setPopupToggle] = useState(false);
+
+	const handlePopupToggle = () => {
+		setPopupToggle(!popupToggle);
+	};
+
 	const bookList = useSelector((state) => {
 		return state.books.bookList;
 	});
+
 	return (
 		<div>
-			<h1 className='font-bold underline'>Blazesoft bookstore test</h1>
+			<h1 className='font-bold underline text-center'>
+				Blazesoft bookstore test
+			</h1>
+
+			<div>
+				<Button
+					onClick={() => {
+						handlePopupToggle();
+					}}
+					className='bg-green-500 text-white text-center p-2'
+				>
+					Add a book
+				</Button>
+			</div>
+			<Popup isOpen={popupToggle} />
 			<ul>
 				{bookList.map((book) => {
 					return <BookCard book={book} />;
