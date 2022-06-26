69be20ce385d150f02815158fa414d633b6f0f68
chungleee <leon.karho.chung@gmail.com>
2022-06-26 16:55:31 -0400

    bookform state, on input change handler func

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import { useDispatch, useSelector, useStore } from "react-redux";
 import BookCard from "../components/BookCard";
 import Button from "../components/Button";
+import InputField from "../components/InputField";
 import Popup from "../components/Popup";
+import TextArea from "../components/TextArea";
 import { addBooks } from "../redux/features/books/booksSlice";
 
 const HomePage = () => {
 	const [popupToggle, setPopupToggle] = useState(false);
+	const [bookForm, setBookForm] = useState({
+		bookName: "",
+		bookPrice: "",
+		bookCategory: "",
+		bookDescription: "",
+	});
 
 	const handlePopupToggle = () => {
 		setPopupToggle(!popupToggle);
+	};
+
+	const handleOnInputChange = (event) => {
+		setBookForm({
+			[event.target.name]: event.target.value,
+		});
 	};
 
 	const bookList = useSelector((state) => {
@@ -18,6 +32,39 @@ const HomePage = () => {
 
 	return (
 		<div>
+			<Popup isOpen={popupToggle} handlePopupToggle={handlePopupToggle}>
+				<form>
+					<div>
+						<label>Name: </label>
+						<input
+							type='text'
+							onChange={handleOnInputChange}
+							name='bookName'
+							value={bookForm.bookName}
+						/>
+					</div>
+					<InputField
+						type='number'
+						onChange={handleOnInputChange}
+						label='Price'
+						name='bookPrice'
+						value={bookForm.bookPrice}
+					/>
+					<InputField
+						type='text'
+						onChange={handleOnInputChange}
+						label='Category'
+						name='bookCategory'
+						value={bookForm.bookCategory}
+					/>
+					<TextArea
+						label='Description'
+						onChange={handleOnInputChange}
+						name='bookDescription'
+						value={bookForm.bookDescription}
+					/>
+				</form>
+			</Popup>
 			<h1 className='font-bold underline text-center'>
 				Blazesoft bookstore test
 			</h1>
@@ -32,7 +79,6 @@ const HomePage = () => {
 					Add a book
 				</Button>
 			</div>
-			<Popup isOpen={popupToggle} />
 			<ul>
 				{bookList.map((book) => {
 					return <BookCard book={book} />;
