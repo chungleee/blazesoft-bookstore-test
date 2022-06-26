8dcc224fd4d1584fda3c6d3ef09f98c9c7ae47ea
chungleee <leon.karho.chung@gmail.com>
2022-06-26 15:41:27 -0400

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../redux/store';
-import '../css/globals.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import "../css/globals.css";
 
 const App = ({ Component, props }) => {
 	return (
 		<Provider store={store}>
-			<Component {...props} />;
+			<Component {...props} />
 		</Provider>
 	);
 };
