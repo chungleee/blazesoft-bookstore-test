cf7483d42a004c42cda2cbe9ebef68b2ae2b07cc
chungleee <leon.karho.chung@gmail.com>
2022-06-27 10:09:06 -0400

    turn form into its own component

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,15 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import BookCard from "../components/BookCard";
 import Button from "../components/Button";
-import InputField from "../components/InputField";
 import Popup from "../components/Popup";
-import TextArea from "../components/TextArea";
-import { addBook } from "../redux/features/books/booksSlice";
+import BookForm from "../components/BookForm";
 
 const HomePage = () => {
-	const dispatch = useDispatch();
-	const [popupToggle, setPopupToggle] = useState(false);
-	const [bookForm, setBookForm] = useState({
-		bookName: "",
-		bookPrice: "",
-		bookCategory: "",
-		bookDescription: "",
-	});
+	const [formPopupToggle, setFormPopupToggle] = useState(false);
 
-	const handlePopupToggle = () => {
-		setPopupToggle(!popupToggle);
-	};
-
-	const handleOnInputChange = (event) => {
-		setBookForm({
-			...bookForm,
-			[event.target.name]: event.target.value,
-		});
-	};
-
-	const handleFormReset = () => {
-		setBookForm({
-			bookName: "",
-			bookPrice: "",
-			bookCategory: "",
-			bookDescription: "",
-		});
-	};
-
-	const handleFormSubmit = (event) => {
-		event.preventDefault();
-		const newBook = {
-			name: bookForm.bookName,
-			price: bookForm.bookPrice,
-			category: bookForm.bookCategory,
-			description: bookForm.bookDescription,
-		};
-		dispatch(addBook(newBook));
-		handleFormReset();
-		handlePopupToggle();
+	const handleFormPopupToggle = () => {
+		setFormPopupToggle(!formPopupToggle);
 	};
 
 	const bookList = useSelector((state) => {
@@ -55,51 +17,9 @@ const HomePage = () => {
 	});
 
 	return (
-		<div>
-			<Popup isOpen={popupToggle}>
-				<form onSubmit={handleFormSubmit}>
-					<InputField
-						type='text'
-						onChange={handleOnInputChange}
-						label='Name'
-						name='bookName'
-						value={bookForm.bookName}
-					/>
-					<InputField
-						type='number'
-						onChange={handleOnInputChange}
-						label='Price'
-						name='bookPrice'
-						value={bookForm.bookPrice}
-					/>
-					<InputField
-						type='text'
-						onChange={handleOnInputChange}
-						label='Category'
-						name='bookCategory'
-						value={bookForm.bookCategory}
-					/>
-					<TextArea
-						label='Description'
-						onChange={handleOnInputChange}
-						name='bookDescription'
-						value={bookForm.bookDescription}
-					/>
-					<div>
-						<Button className='p-2 bg-green-500' type='submit'>
-							Submit
-						</Button>
-						<Button
-							className='bg-white p-2'
-							type='button'
-							onClick={() => {
-								handlePopupToggle();
-							}}
-						>
-							Close popup
-						</Button>
-					</div>
-				</form>
+		<div className='relative'>
+			<Popup isOpen={formPopupToggle}>
+				<BookForm handleFormPopupToggle={handleFormPopupToggle} />
 			</Popup>
 			<h1 className='font-bold underline text-center'>
 				Blazesoft bookstore test
@@ -108,7 +28,7 @@ const HomePage = () => {
 			<div>
 				<Button
 					onClick={() => {
-						handlePopupToggle();
+						handleFormPopupToggle();
 					}}
 					className='bg-green-500 text-white text-center p-2'
 				>
