718e8af6d266e5cd1bd5e298ef49e4fb8cc9e8be
chungleee <leon.karho.chung@gmail.com>
2022-06-27 10:07:51 -0400

    pop up to edit book and its state

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import Button from "../components/Button";
 import { deleteBook } from "../redux/features/books/booksSlice";
+import BookCardPopup from "./BookCardPopup";
+import Popup from "./Popup";
 
 const BookCard = ({ book }) => {
 	const dispatch = useDispatch();
-	return (
-		<div className='border border-solid border-black mb-4'>
-			<div className='flex'>
-				<label>Name: </label>
-				<p>{book.name}</p>
-			</div>
-			<div className='flex'>
-				<label>Price: </label>
-				<p>{book.price}</p>
-			</div>
-			<div className='flex'>
-				<label>Category: </label>
-				<p>{book.category}</p>
-			</div>
-			<div className='flex'>
-				<label>Description: </label>
-				<p>{book.description}</p>
-			</div>
+	const [isOpen, setIsOpen] = useState(false);
 
-			<Button
+	const handleBookCardPopupToggle = () => {
+		setIsOpen(!isOpen);
+	};
+	return (
+		<div>
+			<Popup isOpen={isOpen}>
+				<BookCardPopup
+					book={book}
+					handleBookCardPopupToggle={handleBookCardPopupToggle}
+				/>
+			</Popup>
+			<div
+				className='border border-solid border-black mb-4 cursor-pointer'
 				onClick={() => {
-					dispatch(deleteBook(book));
+					handleBookCardPopupToggle();
 				}}
-				className='bg-red-500 text-white p-2 text-center'
 			>
-				Delete book
-			</Button>
+				<div className='flex'>
+					<label>Name: </label>
+					<p>{book.name}</p>
+				</div>
+				<div className='flex'>
+					<label>Price: </label>
+					<p>{book.price}</p>
+				</div>
+				<div className='flex'>
+					<label>Category: </label>
+					<p>{book.category}</p>
+				</div>
+				<div className='flex'>
+					<label>Description: </label>
+					<p>{book.description}</p>
+				</div>
+
+				<Button
+					onClick={() => {
+						dispatch(deleteBook(book));
+					}}
+					className='bg-red-500 text-white p-2 text-center'
+				>
+					Delete book
+				</Button>
+			</div>
 		</div>
 	);
 };
